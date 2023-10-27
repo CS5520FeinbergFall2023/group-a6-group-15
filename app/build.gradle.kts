974eed7001588609f467bb1914224eea5fbfaaf2
kaiqi zhang <kaiqiz@kaiqis-MacBook-Air.local>
2023-10-27 19:29:57 -0400

    test setValue with A8-group15-new db

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("com.google.android.material:material:<version>")
-    implementation(platform("com.google.firebase:firebase-bom:32.4.0"))
+    implementation(platform("com.google.firebase:firebase-bom:32.4.1"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-database:20.3.0")
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/edu/northeastern/group15_assignment/MainActivity.java
+++ app/src/main/java/edu/northeastern/group15_assignment/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyApp";
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
 
 
@@ -18,11 +20,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        //        todo: test
+//        rootNode = FirebaseDatabase.getInstance();
+////        Log.i(TAG, "print root node in logcat");
+//        System.out.println("print root node");
+//        System.out.println("root node info is down below");
+//        System.out.println(rootNode);
+//        reference = rootNode.getReference("message");
+//        System.out.println(reference);
+////        reference.child("message").setValue("Hello world!!");
+//        reference.setValue("Hello, World!");
+//        reference.setValue("Hello????").addOnCompleteListener(new OnCompleteListener<Void>(){
+//
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task){
+//                // SUCCESS
+//
+//                // Log the details
+//                Log.d("FirebaseData","user data uploaded successfully");
+//                // Make a toast
+//                Toast.makeText(getApplicationContext(), "user data uploaded successfully", Toast.LENGTH_LONG).show();
+//            }
+//
+//        }).addOnFailureListener(new OnFailureListener(){
+//            @Override
+//            public void onFailure(@NonNull Exception e){
+//                // FAILURE
+//
+//                // Log the details
+//                Log.d("FirebaseData","user data upload failed");
+//                // Make a toast
+//                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+//            }
+//        });
+
+//        Read
+//        reference.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DataSnapshot> task) {
+//                if (!task.isSuccessful()) {
+//                    Log.e("firebase", "Error getting data", task.getException());
+//                }
+//                else {
+//                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+//                }
+//            }
+//        });
+
+
     }
 
     public void onAboutClick(View view) {
+
         startActivity(new Intent(this, AboutGroupActivity.class));
 
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference("message");
+        System.out.println("reference");
+        System.out.println(reference);
+
+        reference.setValue("Fire data storage");
+
+        System.out.println("so far so good");
 
     }
 
