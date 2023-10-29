2ad35c2ddb481c18b63b70b53084ade1d0b37ef5
Thameem Abbas <tabbas97@gmail.com>
2023-10-29 16:37:28 -0400

    ADD: ManageFriends, Username validations

---
+++ app/src/main/java/edu/northeastern/group15_assignment/UserFriendsManage.java
@@ -0,0 +1,14 @@
+package edu.northeastern.group15_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserFriendsManage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_friends_manage);
+    }
+}
